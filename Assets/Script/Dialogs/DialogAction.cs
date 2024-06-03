6598c38e101ef01daeea045eb580df233e42c509
Felipe Assis <felipe.assis.2002@gmail.com>
2024-06-03 11:11:51 -0300

    fixing typo

--- Assets/Script/Dialogs/DialogAction.cs
+++ Assets/Script/Dialogs/DialogAction.cs
@@ -8,15 +8,3 @@ public abstract class DialogAction : ScriptableObject
         return "";
     }
 }
-
-[CreateAssetMenu (fileName = "New Go To Dialog", menuName = "Configs/DialogAction/GoToDialog")]
-public abstract class GoToDialog : DialogAction
-{
-    [SerializeField] private DialogConfig desiredDialog;
-    public override string ExecuteAction()
-    {
-        GameMaster.Instance.LoadDialog(desiredDialog);
-
-        return "";
-    }
-}
---
+++ Assets/Script/Dialogs/GoToDialog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Go To Dialog", menuName = "Configs/Dialog Action/GoToDialog")]
+public abstract class GoToDialog : DialogAction
+{
+    [SerializeField] private DialogConfig desiredDialog;
+    public override string ExecuteAction()
+    {
+        GameMaster.Instance.LoadDialog(desiredDialog);
+
+        return "";
+    }
+}
